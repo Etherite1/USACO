92146116b054f5c81f314aa6885fd6491cb5f349
Humphrey Xu <xuhumphrey@gmail.com>
2020-10-19 14:20:34 -0500

    Update dsu.cpp

--- DSA/dsu.cpp
+++ DSA/dsu.cpp
@@ -1,3 +1,13 @@
+/*
+tests connectivity among pairwise edges in O(a(n)) time
+
+functions:
+init(n) - initializes dsu with n nodes
+find(x) - returns root of the tree of node x
+merge(x, y) - merges the sets of x and y, which basically connects the components
+
+*/
+
 struct dsu
 {
     vector<int> parent, sz;
