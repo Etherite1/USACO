3f8bb64a3a52efa26ecf0b2ea9143158d3d310ca
Humphrey Xu <xuhumphrey@gmail.com>
2020-10-19 14:15:14 -0500

    Update dsu.cpp

--- DSA/dsu.cpp
+++ DSA/dsu.cpp
@@ -1,34 +1,22 @@
 struct dsu
 {
     vector<int> parent, sz;
-
     void init(int n)
     {
-        parent = vector<int>(n + 1);
-        sz = vector<int>(n + 1);
-        for(int i = 0; i < n; i++)
-        {
-            parent[i] = i;
-            sz[i] = 1;
-        }
-    }
-    
+        parent = vector<int>(n + 1), sz = vector<int>(n + 1, 1);
+        for(int i = 0; i < n; i++) parent[i] = i;
+    }  
+
     int find(int x)
     {
         if(parent[x] == x) return x;
-        else
-        {
-            parent[x] = find(parent[x]);
-            return parent[x];
-        }
+        else return parent[x] = find(parent[x]);
     }
 
     void merge(int a, int b)
     {
         int roota = find(a), rootb = find(b);
         if(sz[roota] > sz[rootb]) swap(roota, rootb);
-        parent[roota] = rootb;
-        sz[rootb] += sz[roota];
+        parent[roota] = rootb; sz[rootb] += sz[roota];
     }
-
 };
