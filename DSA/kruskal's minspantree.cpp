fdc80753f3026d1d271ff3feddcf861be0437c41
Humphrey Xu <xuhumphrey@gmail.com>
2020-10-25 13:33:17 -0500

    Update kruskal's minspantree.cpp

--- DSA/kruskal's minspantree.cpp
+++ DSA/kruskal's minspantree.cpp
@@ -1,7 +1,39 @@
 /*
+tests connectivity among pairwise edges in O(a(n)) time
+functions:
+init(n) - initializes dsu with n nodes
+find(x) - returns root of the tree of node x
+merge(x, y) - merges the sets of x and y, which basically connects the components
+*/
+
+struct dsu
+{
+    vector<int> parent, sz;
+    void init(int n)
+    {
+        parent = vector<int>(n + 1), sz = vector<int>(n + 1, 1);
+        for(int i = 0; i < n; i++) parent[i] = i;
+    }  
+
+    int find(int x)
+    {
+        if(parent[x] == x) return x;
+        else return parent[x] = find(parent[x]);
+    }
+
+    void merge(int a, int b)
+    {
+        int roota = find(a), rootb = find(b);
+        if(sz[roota] > sz[rootb]) swap(roota, rootb);
+        parent[roota] = rootb; sz[rootb] += sz[roota];
+    }
+};
+
+
+/*
 returns sum of minimum spanning tree in O(v^2 * logv^2) time, with v as number of vertices
 a vector of pair<int, pair<int, int>> must be initialized, this represents pair<weight, pair<endpoint, endpoint>>
-also requires a DSU to function (code provided in other file)
+also requires a DSU to function (code above)
 */
 
 dsu d; d.init(n);
