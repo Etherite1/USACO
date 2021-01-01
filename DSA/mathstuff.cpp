94921ca4e83ae026c7db8f770c306ade2e03b258
Humphrey Xu <xuhumphrey@gmail.com>
2020-12-31 21:21:41 -0600

    Update mathstuff.cpp

--- DSA/mathstuff.cpp
+++ DSA/mathstuff.cpp
@@ -1,3 +1,17 @@
+/*
+number theory template
+includes: 
+    ceil
+    gcd
+    lcm
+    line segment intersection test
+    primality test
+    sieve of eratosthenes
+    prime generation
+    factoring a number
+    obtaining divisors of a number
+*/
+
 
 // ceil 
 ll aceil(ll num, ll denom)
@@ -18,19 +32,96 @@ long long lcm(int a, int b)
 }
 
 // checks whether two line segments intersect
-bool inter(float p0_x, float p0_y, float p1_x, float p1_y, float p2_x, float p2_y, float p3_x, float p3_y)
+bool inter(long double p0_x, long double p0_y, long double p1_x, long double p1_y, long double p2_x, long double p2_y, long double p3_x, long double p3_y)
 {
-    float s1_x, s1_y, s2_x, s2_y;
+    long double s1_x, s1_y, s2_x, s2_y;
     s1_x = p1_x - p0_x;     
     s1_y = p1_y - p0_y;
     s2_x = p3_x - p2_x;     
     s2_y = p3_y - p2_y;
 
-    float s, t;
+    long double s, t;
     s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
     t = ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
 
-    if (s >= 0 && s <= 1 && t >= 0 && t <= 1) return 1;
+    if(s >= 0 && s <= 1 && t >= 0 && t <= 1) return 1;
     else return 0; // No collision
 }
 
+//tests primality of a number
+bool isprime(long long n) 
+{ 
+    if(n <= 1) return 0; 
+	if(n <= 3) return 1; 
+	if(n % 2 == 0 || n % 3 == 0) return 0; 
+	for(long long i = 5; i * i <= n; i += 6) if (n % i == 0 || n % (i+2) == 0) return 0; 
+	return 1; 
+} 
+
+//runs sieve of erathosthenes
+bitset<15000150> prime; 
+void sieve(int n) 
+{ 
+	for(long long i = 0; i <= n; i++) prime[i] = 1;
+	for(long long p = 2; p * p <= n; p++) 
+    { 
+        if(prime[p]) 
+        { 
+            for(long long i = p * p; i <= n; i += p) prime[i] = false; 
+        } 
+	} 
+	prime[1] = prime[0] = 0;
+} 
+	 
+//generates all primes up to and including a number
+bool __primes_generated__ = 0;
+vector<long long> genprimes(int n) 
+{
+    vector<long long> primelist;
+    __primes_generated__ = 1;
+    sieve(n + 1);
+    for(long long i = 2; i <= n; i++) if (prime[i]) primelist.push_back(i);
+    return primelist;
+}
+
+//gets all factors of a number
+vector<long long> factors(long long n) 
+{
+    if(!__primes_generated__) 
+    {
+        cout << "Call genprimes you dope" << endl;
+        exit(1);
+    }
+    vector<long long> facs;
+
+    for(long long i = 0; primelist[i] * primelist[i] <= n && i < primelist.size(); i++) 
+    {
+        if(n % primelist[i] == 0) 
+        {
+            while (n % primelist[i] == 0) 
+            {
+                n /= primelist[i];
+                facs.push_back(primelist[i]);
+            }
+        }
+    }
+    if(n > 1) facs.push_back(n);
+    sort(facs.begin(), facs.end());
+    return facs;
+}
+
+//gets all divisors of a number
+
+vector<long long> divisors(long long n) 
+{ 
+    vector<long long> divs;
+    for(long long i = 1; i * i <= n; i++) 
+    { 
+        if(n % i == 0) 
+        { 
+            if(n / i == i) divs.push_back(i);
+            else divs.push_back(i), divs.push_back(n / i);
+        } 
+    } 
+    return divs;
+} 
