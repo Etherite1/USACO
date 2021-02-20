57d3dc216cd110d8250a58d99b3bf0dd0e889b52
Humphrey Xu <xuhumphrey@gmail.com>
2021-02-19 20:07:00 -0600

    Update mathstuff.cpp

--- DSA/mathstuff.cpp
+++ DSA/mathstuff.cpp
@@ -38,6 +38,12 @@ long long binpow(long long a, long long b, long long m = 1e9 + 7)
     return res;
 }
 
+//computes the modular inverse of a mod m, requires binpow function
+long long modinv(long long a, long long m = 1e9 + 7)
+{
+	return binpow(a, m - 2, m);
+}
+
 // greatest common divisor
 long long gcd(long long a, long long b)
 {
@@ -48,23 +54,6 @@ long long gcd(long long a, long long b)
 long long lcm(long long a, long long b)
 {
     return (a / gcd(a, b)) * b;
-}
-
-// checks whether two line segments intersect
-bool inter(long double p0_x, long double p0_y, long double p1_x, long double p1_y, long double p2_x, long double p2_y, long double p3_x, long double p3_y)
-{
-    long double s1_x, s1_y, s2_x, s2_y;
-    s1_x = p1_x - p0_x;     
-    s1_y = p1_y - p0_y;
-    s2_x = p3_x - p2_x;     
-    s2_y = p3_y - p2_y;
-
-    long double s, t;
-    s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
-    t = ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
-
-    if(s >= 0 && s <= 1 && t >= 0 && t <= 1) return 1;
-    else return 0; // No collision
 }
 
 //tests primality of a number
@@ -163,7 +152,6 @@ vector<long long> factors(long long n)
 }
 
 //gets all divisors of a number
-
 vector<long long> divisors(long long n) 
 { 
     vector<long long> divs;
