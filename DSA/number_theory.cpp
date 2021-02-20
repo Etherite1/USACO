f14d5ffc6c57211ecf7f08cc466d6a19e6f30955
Humphrey Xu <xuhumphrey@gmail.com>
2021-02-19 20:13:56 -0600

    Update number_theory.cpp

--- DSA/number_theory.cpp
+++ DSA/number_theory.cpp
@@ -31,9 +31,9 @@ long long binpow(long long a, long long b, long long m = 1e9 + 7)
 {
     a %= m;
     long long res = 1;
-    while (b > 0) 
+    while(b > 0) 
     {
-        if (b & 1)res = res * a % m;
+        if(b & 1) res = res * a % m;
         a = a * a % m;
         b >>= 1;
     }
