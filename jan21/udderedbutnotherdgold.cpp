5ed6bfc8e6fb30f6d33c53af9402bbdcb30100b8
Humphrey Xu <xuhumphrey@gmail.com>
2021-06-13 22:44:50 -0500

    Create udderedbutnotherdgold.cpp

---
+++ jan21/udderedbutnotherdgold.cpp
@@ -0,0 +1,101 @@
+#include "bits/stdc++.h"
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pipii pair<int, pair<int, int>>
+#define plpll pair<long long, pair<long long, long long>>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<long long>>
+#define vii vector<pii>
+#define vll vector<pll>
+#define vl vector<long long>
+#define vi vector<int>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define endl "\n"
+#define rall(x) (x).rbegin(),(x).rend()
+#define rsz resize
+#define sz(a) ((int)(a).size())
+
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update > oset; // find_by_order, order_of_key
+#define fbo find_by_order
+#define ook order_of_key
+
+#ifdef Etherite_Local
+#define DBG1(x) cerr << (#x) << ": " << (x) << endl;
+#define DBG2(x, y) cerr << (#x) << " " << (#y) << ": " << (x) << " " << y << endl;
+#define DBG3(x, y, z) cerr << (#x) << " " << (#y) << " " << (#z) << ": " << (x) << " " << y << " " << z << endl;
+#define DBG4(a, b, c, d) cerr << (#a) << " " << (#b) << " " << (#c) << " " << (#d) << ": " << a << " " << b << " " << c << " " << d << endl;
+template<class T> void DBGpair(T a, string s = "") {cerr << s << (s == ""? "" : ": ") <<  a.first << ", " << a.second << endl;}
+template <class T> void DBGvec(vector<T> a){for(T i : a) cerr << i << " "; cerr << endl;}
+mt19937 rnd(228);
+#else
+#define DBG1(x)
+#define DBG2(x, y)
+#define DBG3(x, y, z)
+#define DBG4(a, b, c, d)
+template<class T> void DBGpair(T a, string s = "") {return;}
+template <class T> void DBGvec(vector<T> &a){}
+mt19937 rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
+#endif
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+char dir[] = {'R', 'L', 'D', 'U'};
+const int MOD = 1e9 + 7; //998244353
+const ll INF = 1e18;
+
+void setIO(string filename) 
+{
+    if(fopen((filename + ".in").c_str(), "r"))
+    {
+        freopen((filename + ".in").c_str(), "r", stdin);
+        freopen((filename + ".out").c_str(), "w", stdout);
+    }
+}
+
+int a[26][26], dp[(1 << 20)];
+
+void solve()
+{
+    string s; cin >> s;
+    map<char, int> m;
+    for(auto i : s) if(m.find(i) == m.end()) m[i] = sz(m);
+    int n = sz(m);
+    for(int i = 1; i < sz(s); i++) a[m[s[i - 1]]][m[s[i]]]++;
+    dp[0] = 1;
+    for(int mask = 1; mask < (1 << n); mask++)
+    {
+        dp[mask] = sz(s);
+        for(int j = 0; j < n; j++)
+        {
+            if(mask & (1 << j))
+            {
+                int sum = dp[mask ^ (1 << j)];
+                for(int k = 0; k < n; k++) if(mask & (1 << k)) sum += a[j][k];
+                dp[mask] = min(dp[mask], sum);
+            }
+        }
+    }
+    cout << dp[(1 << n) - 1] << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0);
+
+    int t = 1;
+    // cin >> t;
+
+    for(int i = 1; i <= t; i++) solve();
+
+    return 0;
+}
