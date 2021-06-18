d8b89741d51ebc6f99739a3a831e9b6ce1e46baf
Humphrey Xu <xuhumphrey@gmail.com>
2021-06-18 17:16:23 -0500

    Create dishes.cpp

---
+++ feb19/dishes.cpp
@@ -0,0 +1,85 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pipii pair<int, pair<int, int>>
+#define plpll pair<long long, pair<long long, long long>>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<long long>>
+#define vii vector<pii>
+#define vll vector<pll>
+#define vl vector<long long>
+#define vi vector<int>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define endl "\n"
+#define rall(x) (x).rbegin(),(x).rend()
+#define rsz resize
+#define sz(a) ((int)(a).size())
+
+#ifdef Etherite_Local
+#define DBG1(x) cerr << (#x) << ": " << (x) << endl;
+#define DBG2(x, y) cerr << (#x) << " " << (#y) << ": " << (x) << " " << y << endl;
+#define DBG3(x, y, z) cerr << (#x) << " " << (#y) << " " << (#z) << ": " << (x) << " " << y << " " << z << endl;
+#define DBG4(a, b, c, d) cerr << (#a) << " " << (#b) << " " << (#c) << " " << (#d) << ": " << a << " " << b << " " << c << " " << d << endl;
+template<class T> void DBGpair(T a, string s = "") {cerr << s << (s == ""? "" : ": ") <<  a.first << ", " << a.second << endl;}
+template <class T> void DBGvec(vector<T> a){for(T i : a) cerr << i << " "; cerr << endl;}
+mt19937 rnd(228);
+#else
+#define DBG1(x)
+#define DBG2(x, y)
+#define DBG3(x, y, z)
+#define DBG4(a, b, c, d)
+template<class T> void DBGpair(T a, string s = "") {return;}
+template <class T> void DBGvec(vector<T> &a){}
+mt19937 rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
+#endif
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+char dir[] = {'R', 'L', 'D', 'U'};
+const int MOD = 1e9 + 7; //998244353
+const ll INF = 1e9;
+
+void setIO(string filename) 
+{
+    if(fopen((filename + ".in").c_str(), "r"))
+    {
+        freopen((filename + ".in").c_str(), "r", stdin);
+        freopen((filename + ".out").c_str(), "w", stdout);
+    }
+}
+
+void solve()
+{
+    setIO("dishes");
+    int n; cin >> n;
+    vvi stk(n + 10);
+    vi pos(n + 1);
+    int mx = 0;
+    for(int i = 0; i < n; i++)
+    {
+        int x; cin >> x;
+        if(x < mx) {cout << i << endl; return;}
+        for(int j = x; j > 0 && pos[j] == 0; j--) pos[j] = x;
+        while(sz(stk[pos[x]]) && stk[pos[x]].back() < x) mx = stk[pos[x]].back(), stk[pos[x]].pop_back();
+        stk[pos[x]].pb(x);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0);
+
+    int t = 1;
+    //cin >> t;
+
+    for(int i = 1; i <= t; i++) solve();
+
+    return 0;
+}
