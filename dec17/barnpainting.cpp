c2a4f8423dbef045d8d45bfa14a4d984713ebcc5
Humphrey Xu <xuhumphrey@gmail.com>
2021-06-22 23:53:21 -0500

    Create barnpainting.cpp

---
+++ dec17/barnpainting.cpp
@@ -0,0 +1,125 @@
+#include "bits/stdc++.h"
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pipii pair<int, pair<int, int>>
+#define plpll pair<long long, pair<long long, long long>>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<long long>>
+#define vii vector<pii>
+#define vll vector<pll>
+#define vl vector<long long>
+#define vi vector<int>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define endl "\n"
+#define rsz resize
+#define sz(a) ((int)(a).size())
+
+typedef tree<long long, null_type, less<long long>, rb_tree_tag, tree_order_statistics_node_update > oset; // find_by_order, order_of_key
+#define fbo find_by_order
+#define ook order_of_key
+#define bitcount __builtin_popcount
+
+#ifdef Etherite_Local
+#define DBG1(x) cerr << (#x) << ": " << (x) << endl;
+#define DBG2(x, y) cerr << (#x) << " " << (#y) << ": " << (x) << " " << y << endl;
+#define DBG3(x, y, z) cerr << (#x) << " " << (#y) << " " << (#z) << ": " << (x) << " " << y << " " << z << endl;
+#define DBG4(a, b, c, d) cerr << (#a) << " " << (#b) << " " << (#c) << " " << (#d) << ": " << a << " " << b << " " << c << " " << d << endl;
+#define DBG5(a, b, c, d, e) cerr << (#a) << " " << (#b) << " " << (#c) << " " << (#d) << " " << (#e) << ": " << a << " " << b << " " << c << " " << d << " " << e << endl;
+#define DBG6(a, b, c, d, e, f) cerr << (#a) << " " << (#b) << " " << (#c) << " " << (#d) << " " << (#e) << " " << (#f) << ": " << a << " " << b << " " << c << " " << d << " " << e << " " << f << endl;
+template<class T> void DBGpair(T a, string s = "") {cerr << s << (s == ""? "" : ": ") <<  a.first << ", " << a.second << endl;}
+template <class T> void DBGvec(vector<T> a){for(T i : a) cerr << i << " "; cerr << endl;}
+template <class T> void DBGset(set<T> a){for(T i : a) cerr << i << " "; cerr << endl;}
+mt19937 rnd(228);
+#else
+#define DBG1(x)
+#define DBG2(x, y)
+#define DBG3(x, y, z)
+#define DBG4(a, b, c, d)
+#define DBG5(a, b, c, d, e)
+#define DBG6(a, b, c, d, e, f)
+template<class T> void DBGpair(T a, string s = "") {return;}
+template <class T> void DBGvec(vector<T> &a){}
+template <class T> void DBGset(set<T> &a){}
+mt19937 rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
+#endif
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+char dir[] = {'R', 'L', 'D', 'U'};
+const int MOD = 1e9 + 7; //998244353
+const ll INF = 1e9;
+
+void setIO(string filename) 
+{
+    if(fopen((filename + ".in").c_str(), "r"))
+    {
+        freopen((filename + ".in").c_str(), "r", stdin);
+        freopen((filename + ".out").c_str(), "w", stdout);
+    }
+}
+
+vvl adj, dp; vi col;
+int n, k;
+
+void dfs(int curr, int par)
+{
+    for(auto adjnode : adj[curr]) if(adjnode != par) dfs(adjnode, curr);
+    for(int c = 1; c <= 4; c++)
+    {
+        if(dp[curr][c] == 0) continue;
+        for(auto adjnode : adj[curr])
+        {
+            if(adjnode != par)
+            {
+                ll tmp = 0;
+                for(int nc = 1; nc <= 4; nc++) if(nc != c) tmp = (tmp + dp[adjnode][nc]) % MOD;
+                dp[curr][c] = (dp[curr][c] * tmp) % MOD;
+            }
+        }
+    }
+}
+
+void solve()
+{
+    setIO("barnpainting");
+    cin >> n >> k;
+    adj.rsz(n); col.rsz(n);
+    dp.rsz(n, vl(4, 1));
+    for(int i = 1; i < n; i++)
+    {
+        int u, v; cin >> u >> v; u--; v--;
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+    for(int i = 0; i < k; i++)
+    {
+        int a, b; cin >> a >> b; 
+        col[a - 1] = b;
+        for(int j = 1; j <= 3; j++) if(j != b) dp[a - 1][j] = 0;
+    }
+    dfs(0, -1);
+    cout << (dp[0][1] + dp[0][2] + dp[0][3]) % MOD << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0);
+
+    int t = 1;
+    //cin >> t;
+
+    for(int i = 1; i <= t; i++) solve();
+
+    return 0;
+}
